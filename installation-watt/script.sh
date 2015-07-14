794f7fdaeda113b30cd516fd5ef227724460d510
micheleorsi <morsi@map2app.com>
2015-07-15 00:35:56 +0200

    merge csv 2007 and 2012 with shp to obtain a topojson

--- installation-watt/script.sh
+++ installation-watt/script.sh
@@ -2,7 +2,9 @@
 
 # db stuff
 rm working/*.db
+rm working/CA_zip.json
 rm output/*.csv
+rm output/CA_zip.topo.json
 brew install sqlite
 
 # geo stuff
@@ -32,15 +34,20 @@ then
 	unzip "source/tl_2010_06_zcta510.zip" -d source
 fi
 
+# create DB and applied sql commands in order to export csv final files
+sqlite3 working/installation.db < sqlcommands.sql
+
 # create topojson for california
 if [ ! -f "output/CA_zip.topo.json" ]
 then
-	# create a new file called 'CA_zip.json' in 'GeoJSON' format 
+	# create a new file in 'GeoJSON' format called 'CA_zip.json' from file 'source/tl_2010_06_zcta510.shp'
 	ogr2ogr -f "GeoJSON" working/CA_zip.json source/tl_2010_06_zcta510.shp
-	# transform file called 'CA_zip.json' into topojson 'CA_zip.topo.json'
+	# transformation geojson to topojson
 	topojson -p -o output/CA_zip.topo.json working/CA_zip.json
+	# merging trend2007 file with geojson file
+	topojson -e output/trend2007.csv --id-property ZCTA5CE10,ZIPCODE -p -o output/CA_zip_2007.json -- source/tl_2010_06_zcta510.shp
+	# merging trend2007 file with geojson file
+	topojson -e output/trend2012.csv --id-property ZCTA5CE10,ZIPCODE -p -o output/CA_zip_2012.json -- source/tl_2010_06_zcta510.shp
 fi
 
-# create DB and applied sql commands in order to export csv final files
-sqlite3 working/installation.db < sqlcommands.sql
 

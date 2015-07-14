6290d70a955ff3135f7f224d46e1a9e1ab80511b
micheleorsi <morsi@map2app.com>
2015-07-14 19:07:21 +0200

    join with population

--- installation-watt/sqlcommands.sql
+++ installation-watt/sqlcommands.sql
@@ -19,18 +19,16 @@
 -- ) AS T1 INNER JOIN population P1 ON (T1.zipcode=P1."Zip Code ZCTA");
 
 -- easier
-SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, 0) AS TREND, P1."2010 Census Population" AS POPULATION
+SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, 0) AS TREND
 FROM (
 SELECT zipcode, trend FROM installation GROUP BY zipcode
-) AS I1 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2007)
-INNER JOIN population P1 ON (I1.zipcode=P1."Zip Code ZCTA");
+) AS I1 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2007);
 
 -- export trend 2012
 .headers on
 .mode csv
 .output output/trend2012.csv
-SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, I1.TREND1) AS TREND, P1."2010 Census Population" AS POPULATION 
+SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, I1.TREND1) AS TREND
 FROM (
 SELECT zipcode, MAX(trend) AS TREND1 FROM installation GROUP BY zipcode
-) AS I1 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2012)
-INNER JOIN population P1 ON (I1.zipcode=P1."Zip Code ZCTA");
+) AS I1 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2012);

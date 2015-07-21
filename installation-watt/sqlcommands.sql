820578a2eb806872f06d9c0448e621fd90935987
micheleorsi <morsi@map2app.com>
2015-07-21 22:03:10 +0200

    fixed problem with integer division in SQL

--- installation-watt/sqlcommands.sql
+++ installation-watt/sqlcommands.sql
@@ -19,7 +19,7 @@
 -- ) AS T1 INNER JOIN population P1 ON (T1.zipcode=P1."Zip Code ZCTA");
 
 -- easier
-SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, 0) AS TREND, P1."2010 Census Population" AS POPULATION, ifnull(I2.trend, 0)/P1."2010 Census Population" AS RATIO
+SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, 0) AS TREND, P1."2010 Census Population" AS POPULATION, ifnull(I2.trend, 0)*1.0/P1."2010 Census Population" AS RATIO
 FROM (SELECT zipcode, trend FROM installation GROUP BY zipcode) AS I1 
 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2007)
 INNER JOIN population P1 ON (I1.zipcode=P1."Zip Code ZCTA");
@@ -28,7 +28,7 @@ INNER JOIN population P1 ON (I1.zipcode=P1."Zip Code ZCTA");
 .headers on
 .mode csv
 .output output/trend2012.csv
-SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, I1.TREND1) AS TREND, P1."2010 Census Population" AS POPULATION, ifnull(I2.trend, I1.TREND1)/P1."2010 Census Population" AS RATIO
+SELECT I1.zipcode AS ZIPCODE, ifnull(I2.trend, I1.TREND1) AS TREND, P1."2010 Census Population" AS POPULATION, ifnull(I2.trend, I1.TREND1)*1.0/P1."2010 Census Population" AS RATIO
 FROM (SELECT zipcode, MAX(trend) AS TREND1 FROM installation GROUP BY zipcode) AS I1 
 LEFT OUTER JOIN installation AS I2 ON (I1.zipcode=I2.zipcode) AND (I2.year = 2012)
 INNER JOIN population P1 ON (I1.zipcode=P1."Zip Code ZCTA");
